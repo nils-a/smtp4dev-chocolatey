362be946d973cb829af5f3e419d91e597f0dae28
Nils Andresen <nils@nils-andresen.de>
2018-05-26 16:00:08 +0200

    implemented comments from chocolatey-review

--- smtp4dev/tools/chocolateyinstall.ps1
+++ smtp4dev/tools/chocolateyinstall.ps1
@@ -1,4 +1,25 @@
-$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-Install-ChocolateyZipPackage "smtp4dev" "https://github.com/rnwood/smtp4dev/releases/download/v2.0.10/smtp4dev-2.0.10-binaries.zip" "$scriptPath"
-$exePath = Join-Path $scriptPath 'smtp4dev.exe'
-Install-ChocolateyDesktopLink $exePath 
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'smtp4dev'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/rnwood/smtp4dev/releases/download/v2.0.10/smtp4dev-2.0.10-binaries.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  softwareName  = 'smtp4dev*'
+  url           = $url
+
+  checksum      = 'ECEE7262066BE31643B26388536A42EFBF8E50C026A04EE4B61CBB15B64FEB09'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+
+Write-Verbose "Adding Desktop-Link"
+$target = Join-Path -Path $toolsDir -ChildPath 'smtp4dev.exe'
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$shortcut = Join-Path -Path $desktop -ChildPath ($packageName + '.lnk')
+Install-ChocolateyShortcut `
+   -ShortcutFilePath $shortcut `
+   -TargetPath $target
---
+++ smtp4dev/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'smtp4dev'
+Write-Verbose "Removing Desktop-Link"
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$shortcut = Join-Path -Path $desktop -ChildPath ($packageName + '.lnk')
+if(Test-Path $shortcut) {
+  Remove-Item $shortcut
+}
